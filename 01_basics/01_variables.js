6037abbd87377d80117b7503f329c3afa52c0270
hi i am puja <132131571+pujagautam-git@users.noreply.github.com>
2026-01-20 15:03:12 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14567
+let accountEmail = "riya@google.com"
+var accountPassword ="1234"
+accountCity = "jaipur"
+let accountState;
+
+
+accountEmail="priya@google.com"
+accountPassword="1367"
+accountCity="pokhara"
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as newer version
+
+alert(3+
+    3) // code readability should be high 
+    console.log("puja")
+
+    let name= "puja"
+    let age= 19
+    let isLoggedIn = false
+
+    //number= 2 to power 53
+    //biginit
+    //string
+    //boolean
+    //null= standalone value(empty value)
+    //undefine= let state;
+  //object
+
+  console.log(typeof age);
+  console.log(typeof null) //is object
